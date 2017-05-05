e88848f845a0294444b517f154b7c14d76efe622
Trevor Fast <trevor@>
2017-05-05 13:42:09 -0700

    add and pass specs for project.rb and project_spec.rb

---
+++ lib/project.rb
@@ -0,0 +1,51 @@
+class Project
+  attr_accessor(:name, :id)
+
+  define_method(:initialize) do |attributes|
+    @name = attributes.fetch(:name)
+    @id = attributes[:id]
+  end
+
+
+  define_singleton_method (:all) do
+    returned_projects = DB.exec("SELECT * FROM projects;")
+    projects = []
+    returned_projects.each() do |project|
+      name = project.fetch("name")
+      id = project.fetch("id").to_i()
+      projects.push(Project.new({:name => name, :id => id}))
+    end
+    projects
+  end
+
+  define_method(:save) do
+    result = DB.exec("INSERT INTO projects (name) VALUES ('#{name}') RETURNING id;")
+    @id = result.first().fetch("id").to_i()
+  end
+
+  define_method(:==) do |new_project|
+    self.name().==(new_project.name())
+  end
+
+  define_method(:update) do |attributes|
+    @name = attributes.fetch(:name)
+    @id = self.id()
+    DB.exec("UPDATE projects SET name = '#{@name}' WHERE id = #{@id}")
+  end
+
+define_method(:delete) do
+  DB.exec("DELETE FROM projects WHERE id = #{self.id()};")
+end
+
+define_singleton_method(:find) do |id|
+    found_project = nil
+    Project.all.each do |project|
+      if project.id .== id
+        found_project = project
+      end
+    end
+    found_project
+  end
+
+
+end
---
+++ spec/project_spec.rb
@@ -0,0 +1,78 @@
+require "project"
+require "rspec"
+require "spec_helper"
+require "pg"
+require "pry"
+
+
+describe(Project) do
+
+  describe(".all") do
+     it("starts off without any projects") do
+       expect(Project.all()).to(eq([]))
+     end
+   end
+
+  describe('#name') do
+    it("initializes the system to add projects to it") do
+      test_project = Project.new({:name => "Dunderhonung", :id => nil})
+      expect(test_project.name()).to(eq("Dunderhonung"))
+    end
+  end
+
+  describe("#id") do
+   it("lets you read the project ID") do
+     test_project = Project.new({:name => "Dunderhonung", :id => nil})
+     expect(test_project.id()).to(eq(nil))
+   end
+ end
+
+ describe("#save") do
+  it("saves the project to the table") do
+    test_project = Project.new({:name => "Dunderhonung", :id => nil})
+    test_project.save()
+    expect(Project.all()).to(eq([test_project]))
+  end
+end
+
+  describe('#==') do
+    it('is the same project if it has the same id') do
+      test_project =  Project.new({:name => "Dunderhonung", :id => nil})
+      test_project2 = Project.new({:name => "Dunderhonung", :id => nil})
+      expect(test_project).to(eq(test_project2))
+    end
+  end
+
+  describe("#update") do
+    it("lets the project be updated") do
+      test_project = Project.new({:name => "Dunderhonung", :id => nil})
+      test_project.save()
+      test_project.update(:name => "Thunder Honey")
+      expect(test_project.name()).to(eq("Thunder Honey"))
+    end
+  end
+
+  describe("#delete") do
+    it("allows the coordinator to delete projects") do
+      test_project = Project.new({:name => "Dunderhonung", :id => nil})
+      test_project.save()
+      test_project2 = Project.new({:name => "Thunder Honey", :id => nil})
+      test_project2.save()
+      test_project.delete()
+    expect(Project.all()).to(eq([test_project2]))
+    end
+  end
+
+describe(".find") do
+  it("returns a project by name") do
+    test_project =  Project.new({:name => "Dunderhonung", :id => nil})
+    test_project.save()
+    test_project2 = Project.new({:name => "Thunder Honey", :id => nil})
+    test_project2.save()
+   expect(Project.find(test_project2.id())).to(eq(test_project2))
+  end
+end
+
+
+
+end
