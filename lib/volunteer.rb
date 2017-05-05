1b4d8cc341424dae373fe60b8f5067503c10e41c
Trevor Fast <trevor@>
2017-05-05 13:39:29 -0700

    debug delete method and spec so it passes. Also fix issue with accidental plural on all method.

--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -7,13 +7,13 @@ class Volunteer
   end
 
 
-  define_singleton_method(:all) do
+  define_singleton_method (:all) do
     returned_volunteers = DB.exec("SELECT * FROM volunteers;")
     volunteers = []
     returned_volunteers.each() do |volunteer|
       name = volunteer.fetch("name")
       id = volunteer.fetch("id").to_i()
-      volunteer.push(Volunteer.new({:name => name, :id => id}))
+      volunteers.push(Volunteer.new({:name => name, :id => id}))
     end
     volunteers
   end
@@ -23,8 +23,29 @@ class Volunteer
     @id = result.first().fetch("id").to_i()
   end
 
+  define_method(:==) do |new_volunteer|
+    self.name().==(new_volunteer.name())
+  end
 
+  define_method(:update) do |attributes|
+    @name = attributes.fetch(:name)
+    @id = self.id()
+    DB.exec("UPDATE volunteers SET name = '#{@name}' WHERE id = #{@id}")
+  end
 
+define_method(:delete) do
+  DB.exec("DELETE FROM volunteers WHERE id = #{self.id()};")
+end
+
+define_singleton_method(:find) do |id|
+    found_volunteer = nil
+    Volunteer.all.each do |volunteer|
+      if volunteer.id .== id
+        found_volunteer = volunteer
+      end
+    end
+    found_volunteer
+  end
 
 
 end
--- spec/volunteer_spec.rb
+++ spec/volunteer_spec.rb
@@ -13,7 +13,7 @@ describe(Volunteer) do
      end
    end
 
-  describe('#initialize') do
+  describe('#name') do
     it("initializes the system to add volunteers to it") do
       test_volunteer = Volunteer.new({:name => "Lille Skutt", :id => nil})
       expect(test_volunteer.name()).to(eq("Lille Skutt"))
@@ -28,23 +28,50 @@ describe(Volunteer) do
  end
 
  describe("#save") do
-  it("lets you save the volunteer to the table") do
+  it("saves the volunteer to the table") do
     test_volunteer = Volunteer.new({:name => "Lille Skutt", :id => nil})
     test_volunteer.save()
-    expect(Volunteer.all()).to(eq(test_volunteer))
+    expect(Volunteer.all()).to(eq([test_volunteer]))
   end
 end
 
-  # describe('#==') do
-  #   it('is the same volunteer if it has the same id') do
-  #     test_volunteer =  Volunteer.new({:name => "Lille Skutt", :id => nil})
-  #     test_volunteer2 = Volunteer.new({:name => "Lille Skutt", :id => nil})
-  #     expect(test_volunteer).to(eq(test_volunteer2))
-  #   end
-  # end
+  describe('#==') do
+    it('is the same volunteer if it has the same id') do
+      test_volunteer =  Volunteer.new({:name => "Lille Skutt", :id => nil})
+      test_volunteer2 = Volunteer.new({:name => "Lille Skutt", :id => nil})
+      expect(test_volunteer).to(eq(test_volunteer2))
+    end
+  end
 
+  describe("#update") do
+    it("lets the volunteer be updated") do
+      test_volunteer = Volunteer.new({:name => "Lille Skutt", :id => nil})
+      test_volunteer.save()
+      test_volunteer.update(:name => "Lilla Skutt")
+      expect(test_volunteer.name()).to(eq("Lilla Skutt"))
+    end
+  end
 
+  describe("#delete") do
+    it("allows the coordinator to delete volunteers") do
+      test_volunteer = Volunteer.new({:name => "Lille Skutt", :id => nil})
+      test_volunteer.save()
+      test_volunteer2 = Volunteer.new({:name => "Bamse", :id => nil})
+      test_volunteer2.save()
+      test_volunteer.delete()
+    expect(Volunteer.all()).to(eq([test_volunteer2]))
+    end
+  end
 
+describe(".find") do
+  it("returns a volunteer by name") do
+    test_volunteer =  Volunteer.new({:name => "Lille Skutt", :id => nil})
+    test_volunteer.save()
+    test_volunteer2 = Volunteer.new({:name => "Bamse", :id => nil})
+    test_volunteer2.save()
+   expect(Volunteer.find(test_volunteer2.id())).to(eq(test_volunteer2))
+  end
+end
 
 
 
